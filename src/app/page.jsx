be5bbf1a1504e31260642935e85a8fbef4b5d677
droplister <anderson@familymedia.org>
2024-09-07 01:03:29 -0400

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -277,11 +277,11 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            Open-source data and tools for Counterparty (XCP)
+            Open-source data and tools for Counterparty <span className="hidden md:inline">(XCP)</span>
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             <strong className="text-xcpBlue dark:text-zinc-200">21e14</strong> is a suite of websites and tools built to support the
-            Counterparty community on Bitcoin. Whether it be a blockchain
+            Counterparty community<span className="hidden md:inline">( on Bitcoin</span>. Whether it be a blockchain
             explorer, NFT project, or dex trading tool, I build things
             I think help showcase the onchain activity and value of
             Counterparty. ❤️
