c8a45f0a78ddee67edd6635db3ddd6d1ff191f94
droplister <anderson@familymedia.org>
2024-09-07 00:52:28 -0400

    Update

--- src/app/speaking/page.jsx
+++ src/app/speaking/page.jsx
@@ -26,53 +26,74 @@ function Appearance({ title, description, event, cta, href }) {
 export const metadata = {
   title: 'Speaking',
   description:
-    'I’ve spoken at events all around the world and been interviewed for many podcasts.',
+    'Please feel free to reach out if you want to record a podacast or schedule a Twitter Space.',
 }
 
 export default function Speaking() {
   return (
     <SimpleLayout
-      title="I’ve spoken at events all around the world and been interviewed for many podcasts."
-      intro="One of my favorite ways to share my ideas is live on stage, where there’s so much more communication bandwidth than there is in writing, and I love podcast interviews because they give me the opportunity to answer questions instead of just present my opinions."
+      title="I occasionally speak at events or appear on podcasts."
+      intro="Please feel free to reach out if you want to record a podacast or schedule a Twitter Space."
     >
       <div className="space-y-20">
         <SpeakingSection title="Conferences">
           <Appearance
-            href="#"
-            title="In space, no one can watch you stream — until now"
-            description="A technical deep-dive into HelioStream, the real-time streaming library I wrote for transmitting live video back to Earth."
-            event="SysConf 2021"
-            cta="Watch video"
+            href="https://www.non-nft.xyz/"
+            title="Silent Builders"
+            description="Opening talk with Judy Mam, Stellabelle, and Ilan Katin on building during the bear market."
+            event="Non-NFT Summit 2022"
+            cta="Learn more"
           />
           <Appearance
-            href="#"
-            title="Lessons learned from our first product recall"
-            description="They say that if you’re not embarassed by your first version, you’re doing it wrong. Well when you’re selling DIY space shuttle kits it turns out it’s a bit more complicated."
-            event="Business of Startups 2020"
-            cta="Watch video"
+            href="https://tabconf.com/"
+            title="Contributing to Open Source Projects"
+            description="Crash-course in how to collaborate, support development, and give productive feedback."
+            event="TABConf 2019"
+            cta="Learn more"
+          />
+          <Appearance
+            href="https://www.nationalartsclub.org/default.aspx?p=.NETEventView&ID=3864484&qfilter=&type=0&ssid=323204&chgs="
+            title="Introduction to Crypto Art"
+            description="Talk on Crypto Art featuring John Crain, Jess Houlgrave, Jessica Angel, and Louis Parker."
+            event="National Arts Club 2018"
+            cta="Learn more"
+          />
+          <Appearance
+            href="https://www.meetup.com/atlantabitdevs/events/247197251/"
+            title="Tokens on Bitcoin"
+            description="Talk about the Counterparty Project with live demonstration of how to issue new tokens."
+            event="Atlanta Blockchain 2018"
+            cta="Learn more"
           />
         </SpeakingSection>
         <SpeakingSection title="Podcasts">
           <Appearance
-            href="#"
-            title="Using design as a competitive advantage"
-            description="How we used world-class visual design to attract a great team, win over customers, and get more press for Planetaria."
-            event="Encoding Design, July 2022"
-            cta="Listen to podcast"
+            href="https://x.com/i/spaces/1mnGeRQNQqYJX"
+            title="Prehistoric Ordinals"
+            description="Explaining a collectible paper wallet from Casey Rodarmor workshop."
+            event="Scarce City, June 2023"
+            cta="Listen on Twitter"
           />
           <Appearance
-            href="#"
-            title="Bootstrapping an aerospace company to $17M ARR"
-            description="The story of how we built one of the most promising space startups in the world without taking any capital from investors."
-            event="The Escape Velocity Show, March 2022"
-            cta="Listen to podcast"
+            href="https://www.youtube.com/watch?v=gqnrlW2BjUw"
+            title="Unraveling the Mystery Behind Bitcoin NFTs: Past, Present, and Future"
+            description="Discussing Counterparty and Ordinals with Jake Gallen's Guest List."
+            event="Jake Gallen's Guest List, April 2023"
+            cta="Watch on YouTube"
           />
           <Appearance
-            href="#"
-            title="Programming your company operating system"
-            description="On the importance of creating systems and processes for running your business so that everyone on the team knows how to make the right decision no matter the situation."
-            event="How They Work Radio, September 2021"
-            cta="Listen to podcast"
+            href="https://soundcloud.com/artontheblockchain/episode-13-featuring-brian-hoffman-of-open-bazaar-dan-anderson-of-bitcorns"
+            title="Brian Hoffman of Open Bazaar & Dan Anderson of Bitcorns"
+            description="Talking about Bitcorn on AOTB. Hosted by DJ J Scrilla & Cynthia Gayton."
+            event="Art On The Blockchain, April 2018"
+            cta="Listen on Soundcloud"
+          />
+          <Appearance
+            href="https://soundcloud.com/unconfirmed-transactions"
+            title="Unconfirmed Transactions"
+            description="My own podcast on Bitcoin and the goings on with NFTs and other news."
+            event="Unconfirmed Transactions, 2016-2018"
+            cta="Listen on Soundcloud"
           />
         </SpeakingSection>
       </div>
